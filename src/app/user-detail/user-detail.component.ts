88ca814f86f4ddf106cc8cac68d029cacc5d6b3e
vpeter1 <varga.peter.mobil@gmail.com>
2022-01-14 14:07:16 +0100

    v1, test ok

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,36 +1,39 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { User } from '../model/user';
 
+
 @Component({
-  selector: 'app-user-detail',
-  templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.scss']
+    selector: 'app-user-detail',
+    templateUrl: './user-detail.component.html',
+    styleUrls: ['./user-detail.component.scss']
 })
 export class UserDetailComponent implements OnInit {
 
-  /**
-   * FELADAT!
-   * Hozd létre az alábbi Input tulajdonságot.
-   * @var user {User} - Input tulajdonság
-   * @default új User
-   */
+    /**
+     * FELADAT!
+     * Hozd létre az alábbi Input tulajdonságot.
+     * @var user {User} - Input tulajdonság
+     * @default új User
+     */
+    @Input() user: User = new User();
 
+    @Output() delUser: EventEmitter<User> = new EventEmitter();
 
-  @Output() delUser: EventEmitter<User> = new EventEmitter();
+    constructor() { }
 
-  constructor() { }
+    ngOnInit(): void {
+    }
 
-  ngOnInit(): void {
-  }
-
-  /**
-   * FELADAT!
-   * A törlés gombra kattintás esetén lefutó metódus.
-   * Neve: onDelete
-   * Működése: a this.delUser.emit metódust meghívja a kapott user-rel.
-   * @param user {User} - az aktuális felhasználó
-   * @returns {void}
-   */
-
+    /**
+     * FELADAT!
+     * A törlés gombra kattintás esetén lefutó metódus.
+     * Neve: onDelete
+     * Működése: a this.delUser.emit metódust meghívja a kapott user-rel.
+     * @param user {User} - az aktuális felhasználó
+     * @returns {void}
+     */
+    onDelete(user: User): void {
+        this.delUser.emit(this.user);
+    }
 
 }
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -2,47 +2,53 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { User } from '../model/user';
 
 @Component({
-  selector: 'app-user-list',
-  templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+    selector: 'app-user-list',
+    templateUrl: './user-list.component.html',
+    styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
 
-  /**
-   * FELADAT!
-   * Hozd létre az alábbi Input tulajdonságot.
-   * @var users {User[]} - Input tulajdonság
-   * @default []
-   */
-  
-  @Output() delUser: EventEmitter<User> = new EventEmitter();
-  currentUser: User = new User();
+    /**
+     * FELADAT!
+     * Hozd létre az alábbi Input tulajdonságot.
+     * @var users {User[]} - Input tulajdonság
+     * @default []
+     */
+    @Input() users: User[] = [];
 
-  constructor() { }
+    @Output() delUser: EventEmitter<User> = new EventEmitter();
+    currentUser: User = new User();
 
-  ngOnInit(): void {
-  }
+    constructor() { }
 
-  /**
-   * FELADAT!
-   * Az eye gombra kattintás esetén lefutó metódus.
-   * Neve: onSelectUser
-   * Működése: a this.currentUser értékét beállítja a kapott változóra.
-   * @param user {User} - egy felhasználó
-   * @returns {void}
-   */
-  
+    ngOnInit(): void {
+    }
 
-  /**
-   * FELADAT!
-   * Az app-user-detail delUser eseményére lefutó metódus.
-   * Neve: onDeleteUser
-   * Működése:
-   * 1. A this.delUser.emit metódust meghívja a kapott user-rel.
-   * 2. A this.currentUser értékeként egy új User-t ad meg.
-   * @param user {User} - egy felhasználó
-   * @returns {void}
-   */
-  
+    /**
+     * FELADAT!
+     * Az eye gombra kattintás esetén lefutó metódus.
+     * Neve: onSelectUser
+     * Működése: a this.currentUser értékét beállítja a kapott változóra.
+     * @param user {User} - egy felhasználó
+     * @returns {void}
+     */
+    onSelectUser(user: User): void {
+        this.currentUser = user;
+    }
+
+    /**
+     * FELADAT!
+     * Az app-user-detail delUser eseményére lefutó metódus.
+     * Neve: onDeleteUser
+     * Működése:
+     * 1. A this.delUser.emit metódust meghívja a kapott user-rel.
+     * 2. A this.currentUser értékeként egy új User-t ad meg.
+     * @param user {User} - egy felhasználó
+     * @returns {void}
+     */
+    onDeleteUser(user: User): void {
+        this.delUser.emit(user);
+        this.currentUser = new User;
+    }
 
 }
